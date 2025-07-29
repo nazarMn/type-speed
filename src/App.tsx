932e0e236824c44a9f6001420615bd284542707f
MarkivNazar <nazarmn2008@gmail.com>
2025-07-29 20:10:32 +0300

    create

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import TextList from './components/textList/textList'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="App">
+        <TextList />
+      </div>
+  
+    </>
+  )
+}
+
+export default App

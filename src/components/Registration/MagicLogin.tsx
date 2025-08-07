90ce85267b7c413612cfcf8050c4b1f9696ff2d3
MarkivNazar <nazarmn2008@gmail.com>
2025-08-07 23:21:02 +0300

    fixed authorization via MagicLogin by globally changing AuthContext to not reload the page

--- src/components/Registration/MagicLogin.tsx
+++ src/components/Registration/MagicLogin.tsx
@@ -1,29 +1,33 @@
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import axios from 'axios'
+import { AuthContext } from '../context/AuthContext/AuthContext'
 
 export default function MagicLogin() {
   const [params] = useSearchParams()
   const token = params.get('token')
   const navigate = useNavigate()
+  const { login } = useContext(AuthContext)
 
   useEffect(() => {
     if (!token) return
 
-    axios.get('http://localhost:3000/api/me', {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-    .then(res => {
-      localStorage.setItem('token', token!)
-      alert('Вхід успішний через magic link!')
-      navigate('/')
-    })
-    .catch(() => {
-      alert('Недійсне або прострочене посилання')
-    })
-  }, [token])
+    axios
+      .get('http://localhost:3000/api/me', {
+        headers: {
+          Authorization: `Bearer ${token}`,  // Тут має бути Authorization, не просто token
+        },
+      })
+      .then(res => {
+        localStorage.setItem('token', token)
+        login(token, res.data)
+        alert('Вхід успішний через magic link!')
+        navigate('/')
+      })
+      .catch(() => {
+        alert('Недійсне або прострочене посилання')
+      })
+  }, [token, login, navigate])
 
   return (
     <div className="p-10 text-center">
